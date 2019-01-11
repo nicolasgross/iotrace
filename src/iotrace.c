f21a56ffe986ed3499e044db6b66a1b8cc2e48a4
Nicolas Gross <nicolas@fam-gross.org>
2019-01-11 14:37:49 +0100

    Add strace clone from blog post
    https://blog.nelhage.com/2010/08/write-yourself-an-strace-in-70-lines-of-code/
    https://github.com/nelhage/ministrace/tree/for-blog

---
+++ src/iotrace.c
@@ -0,0 +1,70 @@
+#define _POSIX_SOURCE
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+
+
+int do_child(int argc, char **argv);
+int do_trace(pid_t child);
+int wait_for_syscall(pid_t child);
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s prog args\n", argv[0]);
+		exit(1);
+	}
+
+	pid_t child = fork();
+	if (child == 0) {
+		return do_child(argc-1, argv+1);
+	} else {
+		return do_trace(child);
+	}
+}
+
+int do_child(int argc, char **argv) {
+	char *args [argc+1];
+	memcpy(args, argv, argc * sizeof(char*));
+	args[argc] = NULL;
+
+	ptrace(PTRACE_TRACEME);
+	kill(getpid(), SIGSTOP);
+	return execvp(args[0], args);
+}
+
+int do_trace(pid_t child) {
+	int status, syscall, retval;
+	waitpid(child, &status, 0);
+	ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
+	while(1) {
+		if (wait_for_syscall(child) != 0) break;
+
+		syscall = ptrace(PTRACE_PEEKUSER, child, sizeof(long)*ORIG_RAX);
+		fprintf(stderr, "syscall(%d) = ", syscall);
+
+		if (wait_for_syscall(child) != 0) break;
+
+		retval = ptrace(PTRACE_PEEKUSER, child, sizeof(long)*RAX);
+		fprintf(stderr, "%d\n", retval);
+	}
+	return 0;
+}
+
+int wait_for_syscall(pid_t child) {
+	int status;
+	while (1) {
+		ptrace(PTRACE_SYSCALL, child, 0, 0);
+		waitpid(child, &status, 0);
+		if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80) {
+			return 0;
+		}
+		if (WIFEXITED(status)) {
+			return 1;
+		}
+    }
+}
