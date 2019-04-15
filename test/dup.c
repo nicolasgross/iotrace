b0e47ccbdaa2c0e4642402f41a3662892d84e0a4
Nicolas Gross <nicolas@fam-gross.org>
2019-04-15 17:09:16 +0200

    Enhance dup test

--- test/dup.c
+++ test/dup.c
@@ -3,18 +3,23 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define TESTFILE "./test-file.txt"
+
 
 int main() {
-	int fd = open("./test-file.txt", O_RDONLY);
+	int fd = open(TESTFILE, O_WRONLY | O_APPEND | O_CREAT, 0644);
 
 	if (fd < 0) {
-		printf("syscall impl error");
+		printf("error while opening '%s'", TESTFILE);
 		exit(1);
 	}
 
 	int dupped_fd = dup(fd);
 
 	printf("Original fd: %d, dupped fd: %d\n", fd, dupped_fd);
+
+	write(fd, "orig", 4);
+	write(dupped_fd, "dup", 3);
 
 	close(fd);
 	close(dupped_fd);
