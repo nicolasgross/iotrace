605bf4aa19ccf78a5bea1e8884168d5e8d5f03b7
Nicolas Gross <nicolas@fam-gross.org>
2019-05-06 12:56:20 +0200

    Extract time tracking in own functions

--- src/syscall_handler.c
+++ src/syscall_handler.c
@@ -51,53 +51,56 @@ static int read_string(pid_t tracee, char const *base, char *dest,
 	return 0;
 }
 
-static unsigned long long calc_elapsed_ns(struct timespec *start_time,
-                                          struct timespec *current_time) {
+static void save_current_time(struct timespec *start_time, int sc) {
+	if (clock_gettime(USED_CLOCK, start_time)) {
+		fprintf(stderr, "Error while reading start time of %s",
+		        syscall_names[sc]);
+		exit(1);
+	}
+}
+
+static unsigned long long calc_elapsed_ns(struct timespec *start_time, int sc) {
+	struct timespec current_time;
+	if (clock_gettime(USED_CLOCK, &current_time)) {
+		fprintf(stderr, "Error while reading end time of %s", syscall_names[sc]);
+		exit(1);
+	}
 	unsigned long long start_ns = start_time->tv_sec * NANOS +
 	                              start_time->tv_nsec;
-	unsigned long long current_ns = current_time->tv_sec * NANOS +
-	                                current_time->tv_nsec;
+	unsigned long long current_ns = current_time.tv_sec * NANOS +
+	                                current_time.tv_nsec;
 	return current_ns - start_ns;
 }
 
 
+// ==== SYSCALL HANDLERS ====
+
 // ---- open/openat ----
 
-static void handle_open_call(pid_t tracee, bool openat) {
+static void handle_open_call(pid_t tracee, int sc, bool openat) {
 	int name_reg;
 	int flags_reg;
-	char *name;
 	if (openat) {
 		name_reg = RSI;
 		flags_reg = RDX;
-		name = "openat";
 	} else {
 		name_reg = RDI;
 		flags_reg = RSI;
-		name = "open";
 	}
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
 	tmps-> int_a = ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * flags_reg);
 	char const *base = (char const *) ptrace(PTRACE_PEEKUSER, tracee,
 	                                         sizeof(long) * name_reg);
 	if (read_string(tracee, base, tmps->filename_buffer, FILENAME_BUFF_SIZE)) {
-		fprintf(stderr, "Error while reading filename of %s", name);
+		fprintf(stderr, "Error while reading filename of %s", syscall_names[sc]);
 		exit(1);
 	}
-	if (clock_gettime(USED_CLOCK, &tmps->start_time)) {
-		fprintf(stderr, "Error while reading start time of %s", name);
-		exit(1);
-	}
+	save_current_time(&tmps->start_time, sc);
 }
 
-static void handle_open_return(pid_t tracee) {
-	struct timespec current_time;
-	if (clock_gettime(USED_CLOCK, &current_time)) {
-		fprintf(stderr, "%s", "Error while reading end time of open/openat");
-		exit(1);
-	}
+static void handle_open_return(pid_t tracee, int sc) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
-	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, &current_time);
+	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, sc);
 	long ret_fd = ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * RAX);
 	if (ret_fd >= 0) {
 		fd_table_insert(tmps->fd_table, tmps->fd_mutex, ret_fd,
@@ -109,23 +112,15 @@ static void handle_open_return(pid_t tracee) {
 
 // ---- close ----
 
-static void handle_close_call(pid_t tracee) {
+static void handle_close_call(pid_t tracee, int sc) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
 	tmps->int_a = (int) ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * RDI);
-	if (clock_gettime(USED_CLOCK, &tmps->start_time)) {
-		fprintf(stderr, "%s", "Error while reading start time of close");
-		exit(1);
-	}
+	save_current_time(&tmps->start_time, sc);
 }
 
-static void handle_close_return(pid_t tracee) {
-	struct timespec current_time;
-	if (clock_gettime(USED_CLOCK, &current_time)) {
-		fprintf(stderr, "%s", "Error while reading end time of close");
-		exit(1);
-	}
+static void handle_close_return(pid_t tracee, int sc) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
-	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, &current_time);
+	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, sc);
 	g_mutex_lock(tmps->fd_mutex);
 	char const *filename = fd_table_lookup(tmps->fd_table, tmps->int_a);
 	if (filename == NULL) {
@@ -139,24 +134,15 @@ static void handle_close_return(pid_t tracee) {
 
 // ---- read ----
 
-static void handle_read_call(pid_t tracee) {
+static void handle_read_call(pid_t tracee, int sc) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
 	tmps->int_a = (int) ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * RDI);
-    // TODO auslagern in eigene Funktion?
-	if (clock_gettime(USED_CLOCK, &tmps->start_time)) {
-		fprintf(stderr, "%s", "Error while reading start time of read");
-		exit(1);
-	}
+	save_current_time(&tmps->start_time, sc);
 }
 
-static void handle_read_return(pid_t tracee) {
-	struct timespec current_time;
-	if (clock_gettime(USED_CLOCK, &current_time)) {
-		fprintf(stderr, "%s", "Error while reading end time of read");
-		exit(1);
-	}
+static void handle_read_return(pid_t tracee, int sc) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
-	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, &current_time);
+	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, sc);
 	ssize_t ret_bytes = ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * RAX);
 	g_mutex_lock(tmps->fd_mutex);
 	char const *filename = fd_table_lookup(tmps->fd_table, tmps->int_a);
@@ -170,23 +156,15 @@ static void handle_read_return(pid_t tracee) {
 
 // ---- write ----
 
-static void handle_write_call(pid_t tracee) {
+static void handle_write_call(pid_t tracee, int sc) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
 	tmps->int_a = (int) ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * RDI);
-	if (clock_gettime(USED_CLOCK, &tmps->start_time)) {
-		fprintf(stderr, "%s", "Error while reading start time of write");
-		exit(1);
-	}
+	save_current_time(&tmps->start_time, sc);
 }
 
-static void handle_write_return(pid_t tracee) {
-	struct timespec current_time;
-	if (clock_gettime(USED_CLOCK, &current_time)) {
-		fprintf(stderr, "%s", "Error while reading end time of write");
-		exit(1);
-	}
+static void handle_write_return(pid_t tracee, int sc) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
-	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, &current_time);
+	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, sc);
 	ssize_t ret_bytes = ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * RAX);
 	g_mutex_lock(tmps->fd_mutex);
 	char const *filename = fd_table_lookup(tmps->fd_table, tmps->int_a);
@@ -200,26 +178,18 @@ static void handle_write_return(pid_t tracee) {
 
 // ---- pipe/pipe2 ----
 
-static void handle_pipe_call(pid_t tracee, bool pipe2) {
+static void handle_pipe_call(pid_t tracee, int sc, bool pipe2) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
 	tmps->ptr = (int *) ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * RDI);
 	if (pipe2) {
 		tmps->int_a = (int) ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * RSI);
 	}
-	if (clock_gettime(USED_CLOCK, &tmps->start_time)) {
-		fprintf(stderr, "%s", "Error while reading start time of pipe/pipe2");
-		exit(1);
-	}
+	save_current_time(&tmps->start_time, sc);
 }
 
-static void handle_pipe_return(pid_t tracee, bool pipe2) {
-	struct timespec current_time;
-	if (clock_gettime(USED_CLOCK, &current_time)) {
-		fprintf(stderr, "%s", "Error while reading end time of pipe/pipe2");
-		exit(1);
-	}
+static void handle_pipe_return(pid_t tracee, int sc, bool pipe2) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
-	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, &current_time);
+	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, sc);
 	if (ptrace(PTRACE_PEEKUSER, tracee, sizeof(long) * RAX) == 0) {
 		int *pipefd = tmps->ptr;
 		int pipefd_read = ptrace(PTRACE_PEEKDATA, tracee, pipefd, NULL);
@@ -325,56 +295,46 @@ static void handle_execve_return(pid_t tracee) {
 // ---- keep also track of time spent in syscalls  ----
 // ---- that are not considered in file statistics ----
 
-static void handle_unconsidered_call(pid_t tracee) {
+static void handle_unconsidered_call(pid_t tracee, int sc) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
-	if (clock_gettime(USED_CLOCK, &tmps->start_time)) {
-		fprintf(stderr, "%s", "Error while reading start time of unconsidered "
-		        "syscall");
-		exit(1);
-	}
+	save_current_time(&tmps->start_time, sc);
 }
 
-static void handle_unconsidered_return(pid_t tracee, int syscall) {
-	struct timespec current_time;
-	if (clock_gettime(USED_CLOCK, &current_time)) {
-		fprintf(stderr, "%s", "Error while reading end time of unconsidered "
-		        "syscall");
-		exit(1);
-	}
+static void handle_unconsidered_return(pid_t tracee, int sc) {
 	thread_tmps *tmps = thread_tmps_lookup(tracee);
-	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, &current_time);
-	syscall_stat_incr(syscall, elapsed_ns);
+	unsigned long long elapsed_ns = calc_elapsed_ns(&tmps->start_time, sc);
+	syscall_stat_incr(sc, elapsed_ns);
 }
 
 
-void handle_syscall_call(pid_t tracee, int syscall) {
+void handle_syscall_call(pid_t tracee, int sc) {
 	// file statistics
-	switch (syscall) {
+	switch (sc) {
 		case SYS_open:
-			handle_open_call(tracee, false);
+			handle_open_call(tracee, sc, false);
 			return;
 		case SYS_openat:
-			handle_open_call(tracee, true);
+			handle_open_call(tracee, sc, true);
 			return;
 		case SYS_close:
-			handle_close_call(tracee);
+			handle_close_call(tracee, sc);
 			return;
 		case SYS_read:
-			handle_read_call(tracee);
+			handle_read_call(tracee, sc);
 			return;
 		case SYS_write:
-			handle_write_call(tracee);
+			handle_write_call(tracee, sc);
 			return;
 		case SYS_pipe:
-			handle_pipe_call(tracee, false);
+			handle_pipe_call(tracee, sc, false);
 			return;
 		case SYS_pipe2:
-			handle_pipe_call(tracee, true);
+			handle_pipe_call(tracee, sc, true);
 			return;
 	}
 
 	// unconsidered syscalls
-	switch (syscall) {
+	switch (sc) {
 		case SYS_dup:
 		case SYS_dup2:
 		case SYS_dup3:
@@ -384,38 +344,38 @@ void handle_syscall_call(pid_t tracee, int syscall) {
 			handle_fcntl_call(tracee);
 			break;
 	}
-	handle_unconsidered_call(tracee);
+	handle_unconsidered_call(tracee, sc);
 }
 
-void handle_syscall_return(pid_t tracee, int syscall) {
+void handle_syscall_return(pid_t tracee, int sc) {
 	// file statistics
-	switch (syscall) {
+	switch (sc) {
 		case SYS_open:
-			handle_open_return(tracee);
+			handle_open_return(tracee, sc);
 			return;
 		case SYS_openat:
-			handle_open_return(tracee);
+			handle_open_return(tracee, sc);
 			return;
 		case SYS_close:
-			handle_close_return(tracee);
+			handle_close_return(tracee, sc);
 			return;
 		case SYS_read:
-			handle_read_return(tracee);
+			handle_read_return(tracee, sc);
 			return;
 		case SYS_write:
-			handle_write_return(tracee);
+			handle_write_return(tracee, sc);
 			return;
 		case SYS_pipe:
-			handle_pipe_return(tracee, false);
+			handle_pipe_return(tracee, sc, false);
 			return;
 		case SYS_pipe2:
-			handle_pipe_return(tracee, true);
+			handle_pipe_return(tracee, sc, true);
 			return;
 	}
 
 	// unconsidered syscalls
-	handle_unconsidered_return(tracee, syscall);
-	switch (syscall) {
+	handle_unconsidered_return(tracee, sc);
+	switch (sc) {
 		case SYS_dup:
 		case SYS_dup2:
 		case SYS_dup3:
